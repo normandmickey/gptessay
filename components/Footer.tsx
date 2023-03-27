f4beab31025834b4ca26ae66ff3c498ac46e329a
lightninginabox <nmoore@lightninginabox.co>
2023-03-27 00:11:48 +0000

    added buy me a beer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,16 +4,15 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
        <div>
-        Buy me a beer{" "}
+        Buy me a{" "}
         <a
           href="https://www.buymeacoffee.com/normandmicP"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          ChatGPT{" "}
+          Beer{" "}
         </a>
-      </div>
        <div>
         Powered by{" "}
         <a
