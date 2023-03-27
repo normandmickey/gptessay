bcec175db004fd22a0487ecaac18d3cd54f0679a
lightninginabox <nmoore@lightninginabox.co>
2023-03-27 00:08:13 +0000

    updated dropdown

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,18 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
-      <div>
+       <div>
+        Buy me a beer{" "}
+        <a
+          href="https://www.buymeacoffee.com/normandmicP"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >
+          ChatGPT{" "}
+        </a>
+      </div>
+       <div>
         Powered by{" "}
         <a
           href="https://openai.com/blog/chatgpt"
--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ const Home: NextPage = () => {
                         className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
                         onClick={() => {
                           navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
+                          toast("Essay copied to clipboard", {
                             icon: "✂️",
                           });
                         }}
