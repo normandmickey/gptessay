79bd8a210d5f1fcea4b503252e8b3912ed804d67
lightninginabox <nmoore@lightninginabox.co>
2023-03-29 10:26:28 +0000

    added reCaptcha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         nonce: undefined,
       }}>
       <Component {...pageProps} />
-      </GoogleReCaptchaProvider>
+      <GoogleReCaptchaProvider />
     </>
   );
 }
