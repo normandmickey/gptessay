66a4f87985f3bf66aa8cae2f8073700543adae3b
lightninginabox <nmoore@lightninginabox.co>
2023-03-31 03:28:24 +0000

    changed window

--- pages/index.tsx
+++ pages/index.tsx
@@ -157,7 +157,7 @@ const Home: NextPage = () => {
                   Your generated essay
                 </h2>
               </div>
-              <div className="space-y-8 flex flex-col items-center max-w-xl mx-auto">
+              <div className="space-y-8 flex flex-col max-w-xl mx-auto">
                 {generatedBios
                   .substring(generatedBios.indexOf("1") + 3)
                   .split("2.")
